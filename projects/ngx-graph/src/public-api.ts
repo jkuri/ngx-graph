475a9af0109dd944fbe5382f9006167eaf30bdd7
Jan Kuri <jkuri88@gmail.com>
2020-06-18 13:41:56 +0200

    feat(charts): init charts

--- projects/ngx-graph/src/public-api.ts
+++ projects/ngx-graph/src/public-api.ts
@@ -1,7 +1,14 @@
-/*
- * Public API Surface of ngx-graph
- */
+export * from './lib/shared/chart.interface';
+export * from './lib/shared/resize.service';
 
-export * from './lib/ngx-graph.service';
-export * from './lib/ngx-graph.component';
-export * from './lib/ngx-graph.module';
+export * from './lib/line-chart/line-chart.class';
+export * from './lib/line-chart/line-chart.component';
+export * from './lib/line-chart/line-chart.module';
+
+export * from './lib/pie-chart/pie-chart.interface';
+export * from './lib/pie-chart/pie-chart.component';
+export * from './lib/pie-chart/pie-chart.module';
+
+export * from './lib/realtime-chart/realtime-chart.interface';
+export * from './lib/realtime-chart/realtime-chart.component';
+export * from './lib/realtime-chart/realtime-chart.module';
